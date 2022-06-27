0d30c10510d754d0c75f87a78adb7f49fc8c00be
pubiqq <serj.ese@gmail.com>
2022-06-27 23:40:48 +0300

    Prepare next development version

--- buildSrc/src/main/kotlin/LibraryConfig.kt
+++ buildSrc/src/main/kotlin/LibraryConfig.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object LibraryConfig {
     const val Group = "io.github.pubiqq"
-    const val Version = "1.1.0"
+    const val Version = "1.2.0-SNAPSHOT"
 
     const val MinSdk = 14
     const val CompileSdk = 32
