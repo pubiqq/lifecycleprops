64ebe5696de65d178c0d00262511d7e659aa6301
pubiqq <serj.ese@gmail.com>
2024-01-18 22:19:38 +0300

    Update sample app to Material 3

--- sample/src/main/kotlin/com/pubiqq/lifecycleprops/sample/MainActivity.kt
+++ sample/src/main/kotlin/com/pubiqq/lifecycleprops/sample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.pubiqq.lifecycleprops.sample
 
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.pubiqq.lifecycleprops.LifecycleAwareConfigurationApi
 import com.pubiqq.lifecycleprops.LifecycleAwareReadOnlyConfiguration
@@ -41,7 +42,9 @@ class MainActivity : AppCompatActivity() {
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
         Log.d(TAG, "[Activity] onCreate")
 
         binding = MainActivityBinding.inflate(layoutInflater)
