f3fd2205abf2634aeefffee9c936671943d1e938
pubiqq <serj.ese@gmail.com>
2024-12-03 15:51:17 +0300

    Prepare next development version

--- build-logic/library/src/main/kotlin/com/pubiqq/lifecycleprops/build_logic/library/Config.kt
+++ build-logic/library/src/main/kotlin/com/pubiqq/lifecycleprops/build_logic/library/Config.kt
@@ -2,7 +2,7 @@ package com.pubiqq.lifecycleprops.build_logic.library
 
 object Config {
     const val Group = "io.github.pubiqq"
-    const val Version = "3.1.0"
+    const val Version = "3.2.0-SNAPSHOT"
 
     const val MinSdk = 21
     const val CompileSdk = 35
